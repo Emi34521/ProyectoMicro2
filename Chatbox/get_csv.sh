97a659798bded86afda149761cf62181a582d3c3
Code_Runner <62799494+Hecta18@users.noreply.github.com>
2025-11-19 21:21:46 -0600

    Create get_csv.sh to fetch CSV file from URL
    Added a script to download a CSV from Google Sheets.

---
+++ Chatbox/get_csv.sh
@@ -0,0 +1,3 @@
+# para linux (bash):
+wget "https://docs.google.com/spreadsheets/d/1d3RnoKpjoudoDzbbDu_d0ED5AwQKYIWqx271EEZSNaU/export?format=csv" -O ConsumoCasaInteligente.csv
+# para windows: curl -O [URL]
